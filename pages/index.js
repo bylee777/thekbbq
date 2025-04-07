084984d84406f7240f07e538190fc41dfd4bb324
bylee777 <leebueyo@gmail.com>
2025-04-07 00:00:53 -0400

    check

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,36 @@
-import { Footer, Navbar } from '../components';
-import { About, Menu, Hero } from '../sections';
+// import { Footer, Navbar } from '../components';
+// import { About, Menu, Hero } from '../sections';
+
+// const Home = () => (
+//   <div className="bg-primary-black overflow-hidden">
+//     <Navbar />
+//     <Hero />
+//     <div className="relative">
+//       <About />
+//       <Menu />
+//     </div>
+//     <div className="relative" />
+//     <Footer />
+//   </div>
+// );
+
+// export default Home;
 
 const Home = () => (
-  <div className="bg-primary-black overflow-hidden">
-    <Navbar />
-    <Hero />
-    <div className="relative">
-      <About />
-      <Menu />
-    </div>
-    <div className="relative" />
-    <Footer />
+  <div
+    style={{
+      backgroundColor: '#000',
+      color: 'white',
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      fontFamily: 'sans-serif',
+    }}
+  >
+    <h1>Hello from The KBBQ!</h1>
+    <p>If you can see this, your Vercel deployment is working 🎉</p>
   </div>
 );
 
